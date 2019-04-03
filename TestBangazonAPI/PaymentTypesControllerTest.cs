32ce732629f85ae2e6c3b3f87d1b1d721c86b71d
Nick Hansen <nick.hansen13@gmail.com>
2019-04-03 10:18:03 -0500

    tests for g1, ga, post complete

--- TestBangazonAPI/PaymentTypesControllerTest.cs
+++ TestBangazonAPI/PaymentTypesControllerTest.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using TestBangazonAPI.Test;
@@ -37,36 +38,103 @@ namespace TestBangazonAPI
         }
 
         // ----------GET SINGLE for Payment Types---------
-
+        [Fact]
         public async Task Test_Get_One_PaymentTypes()
         {
             using (var client = new APIClientProvider().Client)
             {
+
                 // ARRANGE
+
+                var response = await client.GetAsync("/api/paymenttypes/1");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var paymentType = JsonConvert.DeserializeObject<PaymentType>(responseBody);
 
                 // ACT
 
                 // ASSERT
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(paymentType.Id == 1);
             }
         }
-
         // ----------POST for Payment Types---------
-
+        /*
+        [Fact]
         public async Task Test_Post_PaymentTypes()
         {
             using (var client = new APIClientProvider().Client)
             {
                 // ARRANGE
+                PaymentType testType = new PaymentType
+                {
+                    AcctNumber = 1998832,
+                    Name = "VenmoCard",
+                    CustomerId = 1
+                };
 
                 // ACT
+                var testTypeAsJSON = JsonConvert.SerializeObject(testType);
+
+                var response = await client.PostAsync(
+                    "/api/paymenttypes", new StringContent(testTypeAsJSON, Encoding.UTF8, "application/json"));
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                var newPaymentType = JsonConvert.DeserializeObject<PaymentType>(responseBody);
 
                 // ASSERT
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.Equal("VenmoCard", newPaymentType.Name);
+            }
+        }
+        */
+        // ----------PUT for Payment Types---------
+        [Fact]
+        public async Task Test_Put_PaymentTypes()
+        {
+
+            string newName = "MasterKard";
+
+            using (var client = new APIClientProvider().Client)
+            {
+                // ARRANGE
+
+                PaymentType modifiedType = new PaymentType
+                {
+                    AcctNumber = 1998832,
+                    Name = newName,
+                    CustomerId = 1
+                };
+
+                // ACT
+                var modifiedTypeAsJSON = JsonConvert.SerializeObject(modifiedType);
+
+                var response = await client.PutAsync(
+                    "/api/paymenttypes/1", new StringContent(modifiedTypeAsJSON, Encoding.UTF8, "application/json"));
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+                var getModifiedType = await client.GetAsync("/api/paymenttypes/1");
+                getModifiedType.EnsureSuccessStatusCode();
+
+                string getModifiedTypeContent = await getModifiedType.Content.ReadAsStringAsync();
+                PaymentType updatedType = JsonConvert.DeserializeObject<PaymentType>(getModifiedTypeContent);
+
+                // ASSERT
+
+                Assert.Equal(HttpStatusCode.OK, getModifiedType.StatusCode);
+                Assert.Equal(newName, updatedType.Name);
             }
         }
 
-        // ----------PUT for Payment Types---------
-
-        public async Task Test_Put_PaymentTypes()
+        /*
+                // ----------DELETE for Payment Types---------
+        [Fact]
+        public async Task Test_Delete_PaymentTypes()
         {
             using (var client = new APIClientProvider().Client)
             {
@@ -77,5 +145,6 @@ namespace TestBangazonAPI
                 // ASSERT
             }
         }
+        */
     }
 }
