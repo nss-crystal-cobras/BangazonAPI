0ecec9c59dbc2a60cd69a177773b8305793811b0
anupamagarg <dolly.singla@gmail.com>
2019-04-03 10:59:08 -0500

    Created Model for Department and Employee

---
+++ BangazonAPI/Models/Depatment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BangazonAPI.Models
+{
+    public class Department
+
+    {
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Budget { get; set; }
+        public List<Employee> productTypes = new List<Employee>();
+    }
+
+
+}
---
+++ BangazonAPI/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Employee
+
+    {
+
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Boolean IsSupervisor { get; set; }
+        public int DepartmentId { get; set; }
+
+     
+    }
+
+
+}
